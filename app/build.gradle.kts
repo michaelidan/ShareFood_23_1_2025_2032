effcb372ac0194093f96f962f9babe816c4ee4e3
michaelidan <80638926+michaelidan@users.noreply.github.com>
2024-12-24 03:00:34 +0200

    build.gradle.kts Update Michael 24/12/2024 3:00 AM
    build.gradle.kts, Updated secondly by Michael, on 24/12/2024.
    little more changes.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+
     buildFeatures {
         compose = true
     }
@@ -110,4 +111,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+fun kotlinOptions(function: () -> Unit) {
+
 }
