3fe4f22710d15f81a82207af5ee0bf11fe4db354
michaelidan <michael9090124@gmail.com>
2025-01-26 16:27:35 +0200

    Michael 26_1_2025

--- app/src/main/java/com/example/sharedfood/activity_contact_us.java
+++ app/src/main/java/com/example/sharedfood/activity_contact_us.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class activity_contact_us extends AppCompatActivity {
-    // 26/01/2025 13:56 , 14:30
+    // 26/01/2025 13:56 , 16:27
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
